09b3ddb13f629afcf2e63e21107ba3630b6fff57
stevojed <jedi1014@comcast.net>
2019-07-22 16:51:16 -0400

    someting

---
+++ ConsoleApp6/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Methods
+    {
+        public static double Multiply(int x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = {'a','e','i','o','u' };
+            char[] punctuation = { '~','!','@','#' };
+
+            char[] hello = "this".ToCharArray();
+            string hi = "my";
+            if (!hi.Contains(vowels.ToString()))
+            {
+                Console.WriteLine("this string does not contain a vowel");
+            }
+
+            //string subbed = hello.Substring(0, index);
+
+            //Console.WriteLine(index);
+            //Console.WriteLine(subbed);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
